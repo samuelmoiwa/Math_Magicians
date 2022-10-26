2eb9cb36d9f603bf97eb63a0ff4825bd50377ebc
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-26 17:56:44 +0000

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Calculator from './components/Calculator';
+import App from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
