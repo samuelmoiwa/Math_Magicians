93f86c0520b263b7213725ddacac7614d75e08f0
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-03 18:58:38 +0000

    update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,9 +5,9 @@ function Home() {
     <div className="home">
       <h2 className="get_better">Get better grades in Maths!</h2>
       <p className="home_text">
-        <spam className="home_text_head"> Take charge of your learning.</spam>
         <br />
-        us the maths magic web app to calculate mathematical problems, It will help you
+        Take charge of your learning use the maths magic web app to calculate
+        mathematical problems, It will help you
         <br />
         improve in mathematics and general problem solving.
       </p>
