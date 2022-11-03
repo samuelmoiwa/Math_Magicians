ab0b3a827dc35fbac9b1024d42d28c8ada5f4b66
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-03 17:45:31 +0000

    add Home.js

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2 className="get_better">Get better grades in Maths!</h2>
+      <p className="home_text">
+        <spam className="home_text_head"> Take charge of your learning.</spam>
+        <br />
+        us the maths magic web app to calculate mathematical problems, It will help you
+        <br />
+        improve in mathematics and general problem solving.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
