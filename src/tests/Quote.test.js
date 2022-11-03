a5913274c6414b339a27b14ae26d0297bce3d9e8
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-11-03 23:42:06 +0200

    remove files

--- src/tests/Quote.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Quote from '../components/pages/Quote';
-
-describe('test for creating the quote section', () => {
-  it('created quote page', () => {
-    const view = render(<Quote />);
-    expect(view).toMatchSnapshot();
-  });
-});
